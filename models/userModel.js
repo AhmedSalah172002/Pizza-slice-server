3c5b4edbc5d7aef59eea97516e9d8f4ff0d02606
AhmedSalah172002 <ahmed172002tayel.com>
2023-08-24 20:11:35 +0300

    cyclic22

--- models/userModel.js
+++ models/userModel.js
@@ -73,7 +73,6 @@ userSchema.pre('save', async function (next) {
 const setImageURL = (doc) => {
   if (doc.profileImg) {
     const imageUrl = `${process.env.BASE_URL}/users/${doc.profileImg}`;
-    console.log(imageUrl);
     doc.profileImg = imageUrl;
   }
 };
