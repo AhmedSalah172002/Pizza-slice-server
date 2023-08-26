c9984f312284fe570f77f502056cf61e02d214a1
AhmedSalah172002 <ahmed172002tayel.com>
2023-08-26 06:45:55 +0300

    remove github url

--- services/productService.js
+++ services/productService.js
@@ -19,7 +19,7 @@ exports.resizeImage = asyncHandler(async (req, res, next) => {
     .resize(600, 600)
     .toFormat('jpeg')
     .jpeg({ quality: 95 })
-    .toFile(`${process.env.GITHUB}${filename}`);
+    .toFile(`uploads/products/${filename}`);
   
     req.body.image = filename;
  }
